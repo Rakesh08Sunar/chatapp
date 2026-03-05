e6a16d0b9f154db2f1397632eb4d3b6b5e8e363f
Rakesh Sunar <rakesh_sunar@ildc-inlp.idealake.com>
2026-03-05 15:57:23 +0530

    updated cors origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: ['http://localhost:3002','https://chatapp-rm4ibupiy-rakesh08sunars-projects.vercel.app'],
   });
 
   app.useGlobalPipes(new ValidationPipe());
